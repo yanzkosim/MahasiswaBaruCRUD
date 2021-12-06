bbc09d640744743f41339b8cf8674c724461ba5c
Stefanus Albert Kosim <albertkosim24@gmail.com>
2021-12-06 13:12:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $server = "localhost";
-$user = "id18042071_root"; //id18042071_root
-$password = "D59@j#4ia})uM#qi"; //D59@j#4ia})uM#qi
-$nama_database = "id18042071_pendaftaran_siswa"; //id18042071_pendaftaran_siswa
+$user = "root"; 
+$password = ""; 
+$nama_database = "pendaftaran_siswa";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
