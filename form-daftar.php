4419bdd10cb4dcca790708f33ddcb4ab92104c23
Stefanus Albert Kosim <albertkosim24@gmail.com>
2021-12-23 20:45:13 +0700

    Update form-daftar.php

--- form-daftar.php
+++ form-daftar.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Formulir Pendaftaran Siswa Baru | SMK Coding</title>
     <meta charset="UTF-8">
-    <meta name="author" content="Stefanus Albert
+    <meta name="author" content="Stefanus Albert">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
